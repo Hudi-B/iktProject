47d9785ef05951622601b6bc69c62d6ca22425ce
Hudi-B <vanosskanoss@gmail.com>
2023-10-11 09:48:55 +0200

    update

--- lesson1006/FetchAPI/index.js
+++ lesson1006/FetchAPI/index.js
@@ -1,20 +1,40 @@
-document.getElementById('login').onsubmit = function(event) {
-    event.preventDefault();
-    var email = event.target.elements.email.value;
-    var password = event.target.elements.password.value;
-    var body = JSON.stringify({
-        email: email,
-        password: password,
+document.getElementById('btn').onclick = function(event){
+    event.preventDefault
+    fetch('https://192.168.10.10:8500/Pizza', {
+    method : "GET",
+    mode : "cors",
+    headers : {
+        "Acces-Control-Allow-Origin" : "*"
+    }
     })
-    console.log(email, password)
-    fetch('https://reqres.in/api/login', {
-        method: 'POST',
-        body: body,
-        headers: {
-            'Content-type':'application/json'
-        }
+.then(response => {
+    if (!response.ok) {
+      console.log("szar a szerver faszkalap")
+    }
+    return response.json();
     })
-    .then(function (response) {
-        console.log(response);
+    .then(function(response){
+        return console.log(response)
+    })
+}
+
+document.getElementById('btn1').onclick = function(event1){
+    event1.preventDefault
+    idInput = document.getElementById("idInput").value
+    fetch('https://192.168.10.10:8500/Pizza/' + idInput, {
+    method : "GET",
+    mode : "cors",
+    headers : {
+        "Acces-Control-Allow-Origin" : "*"
+    }
+    })
+.then(response => {
+    if (!response.ok) {
+      console.log("szar a szerver faszkalap")
+    }
+    return response.json();
+    })
+    .then(function(response){
+            console.log(response);
     })
 }
